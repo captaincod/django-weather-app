48de523c96f52f2a0b18d86f0d8edf115f5a1e0b
Olesya Ivanova <iod5000@gmail.com>
2025-05-26 19:41:42 +0800

    Работает автодополнение

--- weather_app/weather/urls.py
+++ weather_app/weather/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "weather"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('autocomplete/', views.autocomplete_city, name='autocomplete_city'),
 ]
--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-import requests
 from .forms import CityForm
 from .models import City
 from .get_weather_table import get_weather_dataframe
 from .visualize_weather import visualize_weather_df_html
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+from weather.models import City
+
+@require_GET
+def autocomplete_city(request):
+    query = request.GET.get('q', '')
+    if query:
+        cities = City.objects.filter(name__iregex=query).values_list('name', flat=True).distinct()[:10]
+        return JsonResponse({'results': list(cities)})
+    return JsonResponse({'results': []})
 
 def index(request):
     weather_plots = None
-    error = None
+    error = ""
+    city = ""
 
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -17,6 +28,7 @@ def index(request):
             city_name = form.cleaned_data['city']
             try:
                 city = City.objects.filter(name__iregex=city_name)[0]
+                print(list(City.objects.filter(name__iregex=city_name)))
                 # TODO: обработка исключений
                 data = get_weather_dataframe(city.latitude, city.longitude)
                 weather_plots = visualize_weather_df_html(data)
@@ -26,6 +38,7 @@ def index(request):
         form = CityForm()
 
     return render(request, 'weather/index.html', {
+        'city': city,
         'form': form,
         'weather_plots': weather_plots,
         'error': error
